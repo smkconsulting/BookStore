1710cdaf814eac7299bf8608e78fc297e3561723
SMK Consulting <smkconsulting@outlook.com>
2023-06-19 20:32:56 +0000

    Created entities and BookStoreDbContext

--- src/BookStore.API/Program.cs
+++ src/BookStore.API/Program.cs
@@ -1,9 +1,13 @@
+using BookStore.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ src/BookStore.Infrastructure/Mappings/CategoryMapping.cs
@@ -0,0 +1,26 @@
+﻿using BookStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Infrastructure.Mappings
+{
+    public class CategoryMapping : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasColumnType("varchar(150)");
+
+            //1 : N => Category : Books
+            builder.HasMany(x=>x.Books).WithOne(x => x.Category).HasForeignKey(x => x.CategoryId);
+
+            builder.ToTable("Categories");
+
+        }
+    }
+}
