96f9bfca6a6fbb12fd4d9f6c64a2b3fdf9b517b2
SMK Consulting <smkconsulting@outlook.com>
2023-06-19 21:47:51 +0000

    Created Services and Interfaces

---
+++ src/BookStore.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Domain.Intefaces;
+using BookStore.Domain.Models;
+using BookStore.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Infrastructure.Repositories
+{
+    public class CategoryRepository: Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(BookStoreDbContext context) : base(context) { }
+    }
+}
